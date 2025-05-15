16850f18ba2486a845f75d283646904d1d081423
KhangLee <150659223+khanhteo1989@users.noreply.github.com>
2025-05-15 18:29:19 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -212,7 +212,9 @@ def upload_file():
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
   
+    if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
